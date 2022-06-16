028374f20256a64434c06a1958d397f153c752c8
PabloValles <valles.pablo@hotmail.com>
2022-06-16 16:35:15 -0300

    Second Commit - primera ejercitaciÂon ok

---
+++ frontend/js/formulario.js
@@ -0,0 +1,96 @@
+window.onload = () => {
+  const query = new URLSearchParams(location.search);
+  const btnAdd = document.querySelector("#btnAdd");
+  const btnEdit = document.querySelector("#btnEdit");
+  const btnDelete = document.querySelector("#btnDelete");
+  const form = document.querySelector(".formulario");
+  let id = document.querySelector("#id");
+  let title = document.querySelector("#title");
+  let rating = document.querySelector("#rating");
+  let awards = document.querySelector("#awards");
+  let release_date = document.querySelector("#release_date");
+  let length = document.querySelector("#length");
+
+  if (query.has("id")) {
+    btnAdd.style.display = "none";
+
+    let movieId = query.get("id");
+    fetch("http://localhost:3031/api/movies/" + movieId)
+      .then((res) => res.json())
+      .then((movie) => {
+        id.value = movie.data.id;
+        title.value = movie.data.title;
+        rating.value = movie.data.rating;
+        awards.value = movie.data.awards;
+        length.value = movie.data.length;
+      })
+      .catch((err) => console.log(err));
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let data = {
+        title: title.value,
+        rating: rating.value,
+        awards: awards.value,
+        release_date: release_date.value,
+        length: length.value,
+        genre_id: 1,
+      };
+
+      let url = "http://localhost:3031/api/movies/update/" + movieId;
+      console.log(url);
+
+      fetch(url, {
+        method: "PUT", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((response) => console.log("Success:", response))
+        .catch((error) => console.error("Error:", error));
+    });
+
+    btnDelete.addEventListener("click", (e) => {
+      let eliminar = confirm("Are you sure you want to delete");
+
+      if (eliminar) {
+        // eliminar movie
+      }
+    });
+
+    //
+  } else {
+    btnEdit.style.display = "none";
+    btnDelete.style.display = "none";
+
+    console.log("Cargar nuevo");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let data = {
+        title: title.value,
+        rating: rating.value,
+        awards: awards.value,
+        release_date: release_date.value,
+        length: length.value,
+        genre_id: 1,
+      };
+
+      let url = "http://localhost:3031/api/movies/create/";
+
+      fetch(url, {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((response) => console.log("Success:", response))
+        .catch((error) => console.error("Error:", error));
+    });
+  }
+};
---
+++ frontend/js/main.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+  const app = document.getElementById("root");
+  const container = document.createElement("div");
+  container.setAttribute("class", "container");
+  app.appendChild(container);
+
+  // Aqui debemos agregar nuestro fetch
+  fetch("http://localhost:3031/api/movies")
+    .then((response) => response.json())
+    .then((peliculas) => {
+      let data = peliculas.data;
+
+      data.forEach((movie) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = movie.title;
+
+        const p = document.createElement("p");
+        p.textContent = `Rating: ${movie.rating}`;
+
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.length}`;
+
+        const link = document.createElement("a");
+        link.setAttribute("href", "formulario.html?id=" + movie.id);
+        link.textContent = "Editar";
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        if (movie.genre !== null) {
+          const genero = document.createElement("p");
+          genero.textContent = `Genero: ${movie.genre.name}`;
+          card.appendChild(genero);
+        }
+        card.appendChild(duracion);
+        card.appendChild(link);
+      });
+    });
+};
